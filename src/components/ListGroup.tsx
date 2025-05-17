355c2dfd21f592b77d3365d5695654e3cade5647
Mohammad Reza Aminian <aminiyan.it@gmail.com>
2025-05-17 10:40:14 +0200

    use fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,42 @@
+import { Fragment } from "react";
 function ListGroup() {
+  //   return (
+  //     // A component cannot return more than one element
+  //     <div>
+  //       <h1>List of items</h1>
+  //       <ul className="list-group">
+  //         <li className="list-group-item">An item</li>
+  //         <li className="list-group-item">A second item</li>
+  //         <li className="list-group-item">A third item</li>
+  //         <li className="list-group-item">A fourth item</li>
+  //         <li className="list-group-item">And a fifth one</li>
+  //       </ul>
+  //     </div>
+  //   );
+
+  //   return (
+  //     <Fragment>
+  //       <h1>List of items</h1>
+  //       <ul className="list-group">
+  //         <li className="list-group-item">An item</li>
+  //         <li className="list-group-item">A second item</li>
+  //         <li className="list-group-item">A third item</li>
+  //         <li className="list-group-item">A fourth item</li>
+  //         <li className="list-group-item">And a fifth one</li>
+  //       </ul>
+  //     </Fragment>
+  //   );
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List of items</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
   );
 }
 
