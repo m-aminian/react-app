b28f7625f489e6db47f2bb9f783e476d47d5d59f
Mohammad Reza Aminian <aminiyan.it@gmail.com>
2025-05-18 22:03:09 +0200

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,25 @@
-import { Fragment } from "react";
 function ListGroup() {
-  const items = ["New York", "San Francisco", "London", "Tokyo", "Berlin"];
+  let items = ["New York", "San Francisco", "London", "Tokyo", "Berlin"];
+  items = [];
+  //   The code below is not good because we have duplication for h1
+  //   if (items.length === 0)
+  //     return (
+  //       <>
+  //         <h1>List of items</h1>
+  //         <p>No item found</p>
+  //       </>
+  //     );
+  //   The below code is not goog because of null
+  //   let message = items.length === 0 ? <p>No item found</p> : null;
+  //   let getMessage = () => {
+  //     return items.length === 0 ? <p>No item found</p> : null;
+  //   };
   return (
     <>
       <h1>List of items</h1>
+      {/* {getMessage()} */}
+      {/* Short-Circuit Rendering */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           // check the error before applying the key prop
