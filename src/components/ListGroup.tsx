7128038423d094ab9d319ef77188f990e3d338b9
Mohammad Reza Aminian <aminiyan.it@gmail.com>
2025-05-18 22:29:12 +0200

    handle events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,29 +1,23 @@
+import type { MouseEvent } from "react";
 function ListGroup() {
   let items = ["New York", "San Francisco", "London", "Tokyo", "Berlin"];
-  items = [];
-  //   The code below is not good because we have duplication for h1
-  //   if (items.length === 0)
-  //     return (
-  //       <>
-  //         <h1>List of items</h1>
-  //         <p>No item found</p>
-  //       </>
-  //     );
-  //   The below code is not goog because of null
-  //   let message = items.length === 0 ? <p>No item found</p> : null;
-  //   let getMessage = () => {
-  //     return items.length === 0 ? <p>No item found</p> : null;
-  //   };
+  const hanldeClick = (event: MouseEvent) => {
+    console.log(event);
+  };
   return (
     <>
       <h1>List of items</h1>
-      {/* {getMessage()} */}
-      {/* Short-Circuit Rendering */}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          // check the error before applying the key prop
-          <li className="list-group-item" key={item}>
+          //   <li
+          //     className="list-group-item"
+          //     key={item}
+          //     onClick={(event) => {
+          //       console.log(event);
+          //     }}
+          //   >
+          <li className="list-group-item" key={item} onClick={hanldeClick}>
             {item}
           </li>
         ))}
