d19290c6cd7a9f6f75919ea128c84d6bf91c3664
Mohammad Reza Aminian <aminiyan.it@gmail.com>
2025-05-20 16:30:33 +0200

    pass children

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 function App() {
-  let items = ["New York", "San Francisco", "London", "Tokyo", "Berlin"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
