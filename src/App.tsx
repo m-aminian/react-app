2007d1d58df2cbfdbf1be9a564578e0021f2bb72
Mohammad Reza Aminian <aminiyan.it@gmail.com>
2025-05-20 16:55:21 +0200

    create reusable button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 function App() {
+  const hanldeClick = () => {
+    console.log("You clicked me");
+  };
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button text="click me" color="danger" onClick={hanldeClick} />
     </div>
   );
 }
