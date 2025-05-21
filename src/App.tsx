92e4d35a746bc679c26e3f4ca313a4863a648002
Mohammad Reza Aminian <aminiyan.it@gmail.com>
2025-05-22 01:56:16 +0200

    understand the state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-// import ListGroup from "./components/ListGroup/ListGroup";
-import ListGroup from "./components/ListGroup";
-import "./App.css";
+import { useState } from "react";
+// React updates state asynchronously meaning not immediately
+// State is stored outside of a component
+// Use hooks at the top level of your component
 function App() {
-  const cities = ["Los Angeles", "Berlin", "Kuala Lumpur"];
-  return <ListGroup items={cities} heading="Cities"></ListGroup>;
+  const [isVisible, setVisibility] = useState(false);
+  let count = 0;
+  const handleClick = () => {
+    setVisibility(true);
+    count++;
+    console.log(isVisible);
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>show</button>
+    </div>
+  );
 }
 
 export default App;
