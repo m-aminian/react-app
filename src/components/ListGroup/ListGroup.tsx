f7cd5353cf59d0c3beb3f46079cf4f90f1298444
Mohammad Reza Aminian <aminiyan.it@gmail.com>
2025-05-22 00:10:53 +0200

    css in js

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,35 +1,44 @@
 import { useState } from "react";
-// so the styles is like a regular javascript object that has all the classes defined in this file.
-import styles from "./ListGroup.module.css";
+import styled from "styled-components";
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  background: ${(props) => (props.active === true ? "blue" : "none")};
+`;
 interface Props {
   items: string[];
   heading: string;
 }
 
+interface ListItemProps {
+  active: boolean;
+}
+
 function ListGroup({ items, heading }: Props) {
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
       <h1>List of {heading}</h1>
       {items.length === 0 && <p>No item found</p>}
-      <ul className={[styles["list-group"], styles.container].join(" ")}>
+      <List>
         {items.map((item, index) => (
-          <li
-            className={
-              index === selectedIndex
-                ? "list-group-item active"
-                : "list-group-item"
-            }
+          <ListItem
+            active={selectedIndex === index}
             key={item}
             onClick={() => {
               setSelectedIndex(index);
             }}
           >
             {item}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
