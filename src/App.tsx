52c75bf0cfbc33d95a09c8c470c3958587290fb2
Mohammad Reza Aminian <aminiyan.it@gmail.com>
2025-05-21 17:49:28 +0200

    exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 function App() {
-  const hanldeClick = () => {
-    console.log("You clicked me");
+  const hanldeShowAlert = () => {
+    setShowAlert(true);
   };
+  const hanldeCloseAlert = () => {
+    setShowAlert(false);
+  };
+  const [showAlert, setShowAlert] = useState(false);
   return (
     <div>
-      <Button text="click me" color="danger" onClick={hanldeClick} />
+      {showAlert && <Alert onClick={hanldeCloseAlert}>This is a warning</Alert>}
+      <Button text="click me" onClick={hanldeShowAlert} />
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,12 +2,24 @@ import type { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  color?: "primary" | "secondary" | "success" | "danger" | "warning";
+  onClick: () => void;
 }
 
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, color = "primary", onClick }: Props) => {
   return (
-    <div className="alert alert-primary" role="alert">
+    <div
+      className={`alert alert-${color} alert-dismissible fade show`}
+      role="alert"
+    >
       {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClick}
+      ></button>
     </div>
   );
 };
