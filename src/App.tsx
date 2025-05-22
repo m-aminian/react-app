efb5f9154014e511519d4b83e3bfe163e59c77a0
Mohammad Reza Aminian <aminiyan.it@gmail.com>
2025-05-22 02:04:26 +0200

    choose the right state structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import { useState } from "react";
 // React updates state asynchronously meaning not immediately
 // State is stored outside of a component
 // Use hooks at the top level of your component
+// avoid redundant state variables(anything that can be computed from the existing state variables)
 function App() {
-  const [isVisible, setVisibility] = useState(false);
-  let count = 0;
-  const handleClick = () => {
-    setVisibility(true);
-    count++;
-    console.log(isVisible);
-  };
-  return (
-    <div>
-      <button onClick={handleClick}>show</button>
-    </div>
-  );
+  // const [firstName, setFirstName] = useState('');
+  // const [lastName, setLastName] = useState('');
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+  });
+  const [isLoading, setLoading] = useState(false);
+  return <div></div>;
 }
 
 export default App;
